bf8df95d03902edc4df0bda5f3929c275fc01e35
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-09-16 16:09:50 +0530

    Project Prototype

---
+++ NumPy/Project/mark_analyzer.py
@@ -0,0 +1,11 @@
+# Project: Student Marks Analyzer
+
+# Build a program that analyzes marks of students in multiple subjects using only NumPy.
+
+# What your project should calculate
+
+# Basic information -> Number of students, Number of subjects, Shape of the dataset
+# What your project should calculate -> Basic information, Number of students, Number of subjects, Shape of the dataset
+# Student-wise analysis -> Total marks of every student, Average marks of every student, Highest mark, Lowest mark, Students scoring above a particular average
+# Subject-wise analysis -> Average marks in each subject, Highest mark in each subject, Lowest mark in each subject, Subject with the highest average
+# Performance analysis -> Overall class average, Highest-performing student, Lowest-performing student, Number of students who passed, Number of students who failed
